0455f1389f34779a82b66e7ac70da89a8304304b
nbogdanov <nbogdanov@bol.com>
2023-05-06 22:28:44 +0200

    Disable Python plugin until all the features are tested

--- src/main/kotlin/com/github/nikolaikopernik/codecomplexity/kotlin/KtLanguageInfoProvider.kt
+++ src/main/kotlin/com/github/nikolaikopernik/codecomplexity/kotlin/KtLanguageInfoProvider.kt
@@ -3,14 +3,15 @@ package com.github.nikolaikopernik.codecomplexity.kotlin
 import com.github.nikolaikopernik.codecomplexity.core.ComplexitySink
 import com.github.nikolaikopernik.codecomplexity.core.ElementVisitor
 import com.github.nikolaikopernik.codecomplexity.core.LanguageInfoProvider
-import com.github.nikolaikopernik.codecomplexity.core.PointType
-import com.github.nikolaikopernik.codecomplexity.core.PointType.*
 import com.intellij.lang.Language
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.lexer.KtToken
-import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtClassInitializer
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.jetbrains.kotlin.psi.KtPropertyAccessor
+import org.jetbrains.kotlin.psi.KtSecondaryConstructor
 
 class KtLanguageInfoProvider(override val language: Language = KotlinLanguage.INSTANCE) : LanguageInfoProvider {
     private val classMemberList = listOf(
@@ -33,5 +34,4 @@ class KtLanguageInfoProvider(override val language: Language = KotlinLanguage.IN
     override fun getVisitor(sink: ComplexitySink): ElementVisitor {
         return KtLanguageVisitor(sink)
     }
-
 }
