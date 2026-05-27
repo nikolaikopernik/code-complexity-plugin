9e2e1c65029f112f63bb2255fd2f4a858e63aee1
LahaLuhem <ahalmehul+git@gmail.com>
2026-05-27 20:39:44 +0200

    Add Dart member and class-body detection to ComplexityInfoProvider

--- src/main/kotlin/com/github/nikolaikopernik/codecomplexity/dart/DartComplexityInfoProvider.kt
+++ src/main/kotlin/com/github/nikolaikopernik/codecomplexity/dart/DartComplexityInfoProvider.kt
@@ -5,27 +5,51 @@ import com.github.nikolaikopernik.codecomplexity.core.ComplexitySink
 import com.github.nikolaikopernik.codecomplexity.core.ElementVisitor
 import com.intellij.lang.Language
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.lang.dart.DartLanguage
+import com.jetbrains.lang.dart.psi.DartClassDefinition
+import com.jetbrains.lang.dart.psi.DartComponent
+import com.jetbrains.lang.dart.psi.DartFactoryConstructorDeclaration
+import com.jetbrains.lang.dart.psi.DartFunctionDeclarationWithBody
+import com.jetbrains.lang.dart.psi.DartFunctionDeclarationWithBodyOrNative
+import com.jetbrains.lang.dart.psi.DartGetterDeclaration
+import com.jetbrains.lang.dart.psi.DartMethodDeclaration
+import com.jetbrains.lang.dart.psi.DartNamedConstructorDeclaration
+import com.jetbrains.lang.dart.psi.DartSetterDeclaration
 
 class DartComplexityInfoProvider(override val language: Language = DartLanguage.INSTANCE) : ComplexityInfoProvider {
 
-    override fun isComplexitySuitableMember(element: PsiElement): Boolean {
-        // TODO: return true for top-level functions and class members. Candidates:
-        // DartMethodDeclaration, DartFunctionDeclarationWithBody,
-        // DartFunctionDeclarationWithBodyOrNative, DartGetterDeclaration,
-        // DartSetterDeclaration, DartFactoryConstructorDeclaration.
-        return false
+    override fun isComplexitySuitableMember(element: PsiElement): Boolean = when (element) {
+        is DartFunctionDeclarationWithBody,
+        is DartFunctionDeclarationWithBodyOrNative,
+        is DartGetterDeclaration,
+        is DartSetterDeclaration,
+        is DartFactoryConstructorDeclaration -> true
+        is DartMethodDeclaration -> !element.isConstructor
+        is DartNamedConstructorDeclaration -> element.hasNonTrivialBody()
+        else -> false
     }
 
     override fun isClassWithBody(element: PsiElement): Boolean {
-        // TODO: return true if element is a DartClassDefinition with at least one member.
-        return false
+        if (element !is DartClassDefinition) return false
+        return PsiTreeUtil.findChildOfAnyType(
+            element,
+            DartMethodDeclaration::class.java,
+            DartGetterDeclaration::class.java,
+            DartSetterDeclaration::class.java,
+            DartFactoryConstructorDeclaration::class.java,
+            DartNamedConstructorDeclaration::class.java
+        ) != null
     }
 
     override fun getVisitor(sink: ComplexitySink): ElementVisitor = DartLanguageVisitor(sink)
 
-    override fun getNameElementFor(element: PsiElement): PsiElement {
-        // TODO: resolve the name identifier for the Dart declaration node.
-        return element
-    }
+    override fun getNameElementFor(element: PsiElement): PsiElement =
+        (element as? DartComponent)?.componentName ?: element
+}
+
+private fun DartNamedConstructorDeclaration.hasNonTrivialBody(): Boolean {
+    val block = functionBody?.block ?: return false
+    val statements = block.statements ?: return false
+    return statements.children.isNotEmpty()
 }
