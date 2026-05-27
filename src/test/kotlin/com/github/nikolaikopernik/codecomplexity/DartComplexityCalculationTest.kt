27b1d78b098d4f3ff155c83ca288156e5416a691
LahaLuhem <ahalmehul+git@gmail.com>
2026-05-27 21:30:48 +0200

    parseTestFile for Dart complexity tests

--- src/test/kotlin/com/github/nikolaikopernik/codecomplexity/DartComplexityCalculationTest.kt
+++ src/test/kotlin/com/github/nikolaikopernik/codecomplexity/DartComplexityCalculationTest.kt
@@ -5,11 +5,21 @@ import com.github.nikolaikopernik.codecomplexity.core.ElementVisitor
 import com.github.nikolaikopernik.codecomplexity.dart.DartComplexityInfoProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.lang.dart.psi.DartArguments
+import com.jetbrains.lang.dart.psi.DartComponent
+import com.jetbrains.lang.dart.psi.DartFactoryConstructorDeclaration
+import com.jetbrains.lang.dart.psi.DartFunctionDeclarationWithBody
+import com.jetbrains.lang.dart.psi.DartFunctionDeclarationWithBodyOrNative
+import com.jetbrains.lang.dart.psi.DartGetterDeclaration
+import com.jetbrains.lang.dart.psi.DartMethodDeclaration
+import com.jetbrains.lang.dart.psi.DartNamedConstructorDeclaration
+import com.jetbrains.lang.dart.psi.DartSetterDeclaration
 
 private const val DART_TEST_FILES_PATH = "src/test/testData/dart"
 
 class DartComplexityCalculationTest : BaseComplexityTest() {
-    // TODO: enable once DartLanguageVisitor is implemented and testData/dart/ has fixtures.
+    // TODO: enable once testData/dart/ has fixtures (Step 13).
     // @Test fun testDartFiles() = checkAllFilesInFolder(DART_TEST_FILES_PATH, ".dart")
 
     override fun getTestDataPath() = DART_TEST_FILES_PATH
@@ -17,6 +27,29 @@ class DartComplexityCalculationTest : BaseComplexityTest() {
     override fun createLanguageElementVisitor(sink: ComplexitySink): ElementVisitor =
         DartComplexityInfoProvider().getVisitor(sink)
 
-    override fun parseTestFile(file: PsiFile): List<Triple<PsiElement, String, Int>> =
-        TODO("Extract Dart methods and their expected @complexity(N) annotation values")
+    override fun parseTestFile(file: PsiFile): List<Triple<PsiElement, String, Int>> {
+        val declarations = PsiTreeUtil.findChildrenOfAnyType(
+            file,
+            DartMethodDeclaration::class.java,
+            DartFunctionDeclarationWithBody::class.java,
+            DartFunctionDeclarationWithBodyOrNative::class.java,
+            DartGetterDeclaration::class.java,
+            DartSetterDeclaration::class.java,
+            DartFactoryConstructorDeclaration::class.java,
+            DartNamedConstructorDeclaration::class.java
+        )
+        return declarations.mapNotNull { decl ->
+            val component = decl as DartComponent
+            val complexity = component.complexityAnnotationValue() ?: return@mapNotNull null
+            val name = component.componentName?.text ?: return@mapNotNull null
+            Triple(decl as PsiElement, name, complexity)
+        }
+    }
+
+    private fun DartComponent.complexityAnnotationValue(): Int? {
+        val metadata = metadataList.firstOrNull { it.referenceExpression?.text == "complexity" } ?: return null
+        val args = PsiTreeUtil.findChildOfType(metadata, DartArguments::class.java) ?: return null
+        val firstArg = args.argumentList?.expressionList?.firstOrNull() ?: return null
+        return firstArg.text.toIntOrNull()
+    }
 }
